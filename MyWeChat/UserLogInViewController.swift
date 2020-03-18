dc01e458614fb092dff71aa5728cbcbc538f5eee
thomas10011 <tanhuiri@126.com>
2020-03-18 17:48:03 +0800

    添加了背景图、登录注册按钮。 做好了主界面，

---
+++ MyWeChat/UserLogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserLogInViewController.swift
+//  MyWeChat
+//
+//  Created by thomas on 2020/3/18.
+//  Copyright © 2020 thomas. All rights reserved.
+//
+
+import UIKit
+
+class UserLogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
